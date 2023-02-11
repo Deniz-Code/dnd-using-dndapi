afb4089afcf8b51b49c26a4748a78e8abb2ae975
ThiefGang <104874053+ThiefGang@users.noreply.github.com>
2023-02-11 10:27:59 +0200

    add setInventory hook to adjust state of inventory on click

--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -1,14 +1,17 @@
+
+
 const InventoryList = (props) => {
   return (
     <div>
       <h3>Inventory List</h3>
-      <ul>{props.inventory.map(item=>
-        (<li key={item.id}>
-          <p>{item.name}</p>
-          <p>Cost:{item.cost}</p>
-        </li>
-
-        ))}</ul>
+      <ul>
+        {props.inventory.map((item) => (
+          <li key={item.id}>
+            <p>{item.name}</p>
+            <p>Cost:{item.cost}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react"
 import { inventoryData } from "../../data/data"
 import InventoryList from "../../components/InventoryList/InventoryList"
 
 const Shop = () => {
-  console.log("Inventory Data:", inventoryData)
+  //create state variable
+  const [inventory, setInventory] = useState(inventoryData)
+
+const handleAddItem=()=>{
+  const newItemOne={id:62555,name:"Magic Wand",cost:1,weight:2}
+  const newItemTwo={id:63444,name:"Book of Spells",cost:15,weight:5}
+
+  setInventory([newItemOne,newItemTwo,...inventory])
+}
+
   return (
     <main>
       <h1>Shop</h1>
-
-      <InventoryList inventory={inventoryData} />
+      <button onClick={handleAddItem}>Click Here</button>
+      {/* before was inventoryData , now its inventory(state)so it changes */}
+      <InventoryList inventory={inventory} />
     </main>
   )
 }
