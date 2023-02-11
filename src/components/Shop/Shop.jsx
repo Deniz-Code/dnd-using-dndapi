7d942d0d5c68fd76fe3cc6e77c53cead48af8c10
ThiefGang <104874053+ThiefGang@users.noreply.github.com>
2023-02-11 09:18:14 +0200

    add inventory list to display

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,7 +1,19 @@
+import { inventoryData } from "../../data/data"
+
 const Shop = () => {
+  console.log("Inventory Data:", inventoryData)
   return (
     <main>
       <h1>Shop</h1>
+
+      <ul>
+        {inventoryData.map((item) => (
+          <li key={item.id}>
+            <p>{item.name}</p>
+            <p> Cost: {item.cost} </p>
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
